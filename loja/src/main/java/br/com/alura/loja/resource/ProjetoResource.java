80578918316db068a4eaa7d9d94b957570978d2a
André Chaves <andrechavesg@gmail.com>
2018-05-18 17:52:32 -0300

    capitulo7

--- loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
+++ loja/src/main/java/br/com/alura/loja/resource/ProjetoResource.java
@@ -1,10 +1,17 @@
 package br.com.alura.loja.resource;
 
+import java.net.URI;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.ProjetoDAO;
 import br.com.alura.loja.modelo.Projeto;
@@ -13,10 +20,19 @@ import br.com.alura.loja.modelo.Projeto;
 public class ProjetoResource {
 
 	@Path("{id}")
-    @GET
-    @Produces(MediaType.APPLICATION_XML)
-    public String busca(@PathParam("id") long id) {
-        Projeto projeto = new ProjetoDAO().busca(id);
-        return projeto.toXML();
-    }
+	@GET
+	@Produces(MediaType.APPLICATION_XML)
+	public String busca(@PathParam("id") long id) {
+		Projeto projeto = new ProjetoDAO().busca(id);
+		return projeto.toXML();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response adiciona(String conteudo) {
+		Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+		new ProjetoDAO().adiciona(projeto);
+		URI uri = URI.create("/projetos/" + projeto.getId());
+		return Response.created(uri).build();
+	}
 }
